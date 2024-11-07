52ad94fca7bd89e33611ba37482d5cb1deb4419b
han <ed114324@edah.org.tw>
2024-11-07 17:05:18 +0800

    喵喵喵

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>ฅ/ᐠΦ౪Φᐟ\ฅ 喵</code>
+          <code>ฅ/ᐠΦ౪Φᐟ\ฅ 喵喵喵</code>
         </p>
         <a
           className="App-link"
